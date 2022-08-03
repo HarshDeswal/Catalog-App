874ca7d03280647c29dc151c00f5467b9c8ff4c7
harshdeswal <harshdeswal03@yahoo.com>
2022-08-03 22:25:26 +0530

    Day3

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -5,19 +5,17 @@ import 'package:flutter/material.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
-  const HomePage({Key? key}) : super(key: key);
-
+  final int days = 30;
+  final String name = "Codepur";
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Catalog App"),
       ),
-      body: Material(
-        child: Center(
-          child: Container(
-            child: Text("Welcomes you to the Jungle"),
-          ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome to $days days of flutter by $name"),
         ),
       ),
       drawer: Drawer(),
