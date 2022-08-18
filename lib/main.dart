d6d2dc03f061d34b5d6ad285f821020a43ee53fa
harshdeswal <harshdeswal03@yahoo.com>
2022-08-18 23:27:36 +0530

    theme completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:my_first_app/Pages/home_page.dart';
 import 'package:my_first_app/Pages/login_page.dart';
 import 'package:my_first_app/utils/routes.dart';
+import 'package:my_first_app/widgets/themes.dart';
 
 void main() {
   runApp(MyApp());
@@ -12,10 +13,8 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       themeMode: ThemeMode.light,
-      theme: ThemeData(primarySwatch: Colors.deepPurple),
-      darkTheme: ThemeData(
-        brightness: Brightness.dark,
-      ),
+      theme: MyTheme.lightTheme(context),
+      darkTheme: MyTheme.darkTheme(context),
       debugShowCheckedModeBanner: false,
       initialRoute: MyRoutes.homeRoute,
       routes: {
